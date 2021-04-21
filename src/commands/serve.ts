3115c627be3a4e112d7c144caaa01310ae59a633
jacquelineturof <jacquelineturof@gmail.com>
2021-04-21 12:04:33 -0400

    serve error handling

--- src/commands/serve.ts
+++ src/commands/serve.ts
@@ -6,11 +6,19 @@ export const serveCommand = new Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p --port <number>', 'port to run server on', '4005')
-    .action((filename = 'notebook.js', options: { port: string}) => {
+    .action(async (filename = 'notebook.js', options: { port: string }) => {
         try {
             const dir = path.join(process.cwd(), path.dirname(filename))
-            serve(parseInt(options.port), path.basename(filename), dir)
+            await serve(parseInt(options.port), path.basename(filename), dir)
+            console.log(
+                `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+            )
         } catch (err) {
-            console.log('err: ', err.message)
+            if (err.code === 'EADDRINUSE') {
+                console.error('Port is in use. Try rrunning on a different port.')
+            } else {
+                console.log('err: ', err.message)
+            }
+            process.exit(1)
         } 
     })
